40e43812a62d0d1d60b2952e364b2899da27fad1
Hashmat <hashmatullah@mpokket.com>
2023-06-15 19:39:40 +0530

    File management first commit

---
+++ src/com/mpokket/model/UserDetails.java
@@ -0,0 +1,12 @@
+package com.mpokket.model;
+import com.mpokket.model.MainMenuOptions;
+
+public class UserDetails {
+
+    public void  getUserDetails(){
+
+        System.out.println("This application is developed by M H Azmat");
+        MainMenuOptions.dispalymainMenu();
+
+    }
+}
